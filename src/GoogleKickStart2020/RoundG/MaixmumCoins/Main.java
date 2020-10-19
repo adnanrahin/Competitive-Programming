fdd4450d3a3e4f083bfb2057250950cf8442c2e5
Arx1971 <adnan.rahin13@gmail.com>
2020-10-18 22:44:34 -0400

    Maximum Coins In the Grid

---
+++ src/GoogleKickStart2020/RoundG/MaixmumCoins/Main.java
@@ -0,0 +1,79 @@
+package GoogleKickStart2020.RoundG.MaixmumCoins;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        FastReader fr = new FastReader();
+
+        int testCase = fr.nextInt();
+
+        for (int tt = 1; tt <= testCase; tt++) {
+            int n = fr.nextInt();
+            int[][] grid = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    grid[i][j] = fr.nextInt();
+                }
+            }
+            solution(grid, testCase);
+        }
+    }
+
+    public static void solution(int[][] grid, int testCase) {
+
+        int maximumCoins = 0;
+
+        System.out.println("Case #" + testCase + ": " + maximumCoins);
+
+    }
+
+}
