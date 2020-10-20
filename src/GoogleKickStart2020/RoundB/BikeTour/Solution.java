f5d463f5ceba209c63a45f8f1ed84f0f115eeff4
Arx1971 <adnan.rahin13@gmail.com>
2020-10-20 17:43:19 -0400

    Finding Peak Point

---
+++ src/GoogleKickStart2020/RoundB/BikeTour/Solution.java
@@ -0,0 +1,77 @@
+package GoogleKickStart2020.RoundB.BikeTour;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+
+        int testCase = fr.nextInt();
+
+        for (int i = 1; i <= testCase; i++) {
+            int size = fr.nextInt();
+            int[] arr = new int[size];
+            for (int j = 0; j < size; j++) arr[j] = fr.nextInt();
+            solution(arr, i);
+        }
+    }
+
+    public static void solution(int[] arr, int testCase) {
+        int counter = 0;
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
+                counter++;
+            }
+        }
+        System.out.println("Case #" + testCase + ": " + counter);
+    }
+
+}
