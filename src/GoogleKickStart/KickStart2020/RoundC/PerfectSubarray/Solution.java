ec67041a9f1bd89d6031ea571d898d790238aa89
Arx1971 <adnan.rahin13@gmail.com>
2020-10-22 03:10:31 -0400

    Perfect Squares Dynamic Programming

---
+++ src/GoogleKickStart/KickStart2020/RoundC/PerfectSubarray/Solution.java
@@ -0,0 +1,76 @@
+package GoogleKickStart.KickStart2020.RoundC.PerfectSubarray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        FastReader fr = new FastReader();
+
+        int testCase = fr.nextInt();
+
+        for (int tt = 1; tt <= testCase; tt++) {
+            int size = fr.nextInt();
+            int[] arr = new int[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = fr.nextInt();
+            }
+        }
+    }
+
+    public static void solution(int[] arr, int caseNumber) {
+        int counter = 0;
+
+
+
+        System.out.println("Case #" + caseNumber + ": " + counter);
+    }
+
+}
