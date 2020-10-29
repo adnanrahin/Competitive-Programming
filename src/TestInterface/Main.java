e590d31e50ecfe2f15afdf59583904fa4004e1d9
Arx1971 <adnan.rahin13@gmail.com>
2020-10-28 22:46:27 -0400

    update

--- src/TestInterface/Main.java
+++ src/TestInterface/Main.java
@@ -8,6 +8,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
     private static final Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
