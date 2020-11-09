ad4559ffbcf115f103a01d09dbcb7e28a8a35ba6
Arx1971 <adnan.rahin13@gmail.com>
2020-11-09 15:31:21 -0500

    Codejam 2016 Problem

---
+++ src/GoogleKickStart/KickStart2016/RoundD/Vote/Solution.java
@@ -0,0 +1,74 @@
+package GoogleKickStart.KickStart2016.RoundD.Vote;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        FastReader fr = new FastReader();
+
+        int testCase = fr.nextInt();
+
+        for (int tt = 1; tt <= testCase; tt++) {
+            int a = fr.nextInt(), b = fr.nextInt();
+            solution(a, b, testCase);
+        }
+
+    }
+
+    public static void solution(int a, int b, int testCase) {
+        double ans = 0.0;
+
+        ans = (a - b) / (double) (a + b);
+
+        System.out.print("Case #" + testCase + ": " + ans);
+    }
+
+}
