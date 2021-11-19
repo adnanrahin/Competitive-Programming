2c02a28859295586022a9a25e0e6143a46850b9f
adnanrahin <adnan.rahin13@gmail.com>
2021-11-19 01:20:32 -0500

    Longest Commong Prefix

---
+++ src/LeetCode/LongestCommonPrefix/Main.java
@@ -0,0 +1,19 @@
+package LeetCode.LongestCommonPrefix;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+
+        public String longestCommonPrefix(String[] strs) {
+
+
+            return "";
+        }
+
+    }
+
+}
