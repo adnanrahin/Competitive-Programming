664e1e9bbd3df3c49426fd2c7c931d5e038a0737
adnanrahin <adnan.rahin13@gmail.com>
2021-11-20 04:32:08 -0500

    Symmetric Tree

---
+++ src/LeetCode/SymmetricTree/Main.java
@@ -0,0 +1,42 @@
+package LeetCode.SymmetricTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            return isSymmetric(root.left, root.right);
+        }
+
+        public boolean isSymmetric(TreeNode left, TreeNode right) {
+            if (left == null && right == null) return true;
+            if (left == null || right == null) return false;
+            if (left.val != right.val) return false;
+            return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        }
+    }
+
+}
