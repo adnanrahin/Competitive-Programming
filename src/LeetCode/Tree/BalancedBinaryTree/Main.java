1c69e563aea69d62be4ae89e6d408b51da035abb
adnanrahin <adnan.rahin13@gmail.com>
2021-11-22 18:39:24 -0500

    Balanced Binray Tree

---
+++ src/LeetCode/Tree/BalancedBinaryTree/Main.java
@@ -0,0 +1,34 @@
+package LeetCode.Tree.BalancedBinaryTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public boolean isBalanced(TreeNode root) {
+
+        }
+    }
+
+}
