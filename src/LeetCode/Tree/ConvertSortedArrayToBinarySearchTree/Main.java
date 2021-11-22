3ca78c9cc6bf7ade99e7cac1249e8a74b30570cc
adnanrahin <adnan.rahin13@gmail.com>
2021-11-22 17:52:21 -0500

    update

---
+++ src/LeetCode/Tree/ConvertSortedArrayToBinarySearchTree/Main.java
@@ -0,0 +1,37 @@
+package LeetCode.Tree.ConvertSortedArrayToBinarySearchTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+    
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public TreeNode sortedArrayToBST(int[] nums) {
+            TreeNode result = new TreeNode();
+
+
+            return result;
+        }
+    }
+
+}
