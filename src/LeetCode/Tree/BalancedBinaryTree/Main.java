7262ea4c2af7ef85a791c26363841a7809161ece
adnanrahin <adnan.rahin13@gmail.com>
2021-11-22 19:40:47 -0500

    Balanced Binary Tree

--- src/LeetCode/Tree/BalancedBinaryTree/Main.java
+++ src/LeetCode/Tree/BalancedBinaryTree/Main.java
@@ -28,7 +28,21 @@ public class Main {
     class Solution {
         public boolean isBalanced(TreeNode root) {
 
+            if (root == null) return true;
+
+            int leftH = getDepth(root.left);
+            int rightH = getDepth(root.right);
+
+            if (Math.abs(leftH - rightH) > 1) return false;
+
+            return isBalanced(root.left) && isBalanced(root.right);
         }
+
+        public int getDepth(TreeNode root) {
+            if (root == null) return 0;
+            return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
+        }
+
     }
 
 }
