93a25dc3c7470ec1771a64cd66bde26fde53ffb3
adnanrahin <adnan.rahin13@gmail.com>
2021-11-23 15:17:58 -0500

    Roman To Integer

---
+++ src/LeetCode/String/IntegerToRomans/Main.java
@@ -0,0 +1,27 @@
+package LeetCode.String.IntegerToRomans;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String intToRoman(int num) {
+
+        int[] val = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < val.length && num > 0; i++) {
+
+            while (val[i] <= num) {
+                num -= val[i];
+                sb.append(symbols[i]);
+            }
+
+        }
+
+        return sb.toString();
+    }
+
+}
