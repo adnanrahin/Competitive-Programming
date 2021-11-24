6246493d9840e08a9b4882164032d0a4b1f2288f
adnanrahin <adnan.rahin13@gmail.com>
2021-11-24 02:30:42 -0500

    update

---
+++ src/LeetCode/LinkedList/SwapNodesInPairs/Main.java
@@ -0,0 +1,50 @@
+package LeetCode.LinkedList.SwapNodesInPairs;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode swapPairs(ListNode head) {
+
+            ListNode headReference = head;
+            ListNode prev = null;
+            ListNode hand = head.next;
+            while (headReference != null && headReference.next != null) {
+                ListNode cur = headReference.next;
+                headReference.next = headReference.next.next;
+                cur.next = headReference;
+                if(prev != null){
+                    prev.next = cur;
+                }
+                prev = headReference;
+                headReference = headReference.next;
+            }
+
+            hand.next = head;
+
+            return hand;
+        }
+    }
+
+}
