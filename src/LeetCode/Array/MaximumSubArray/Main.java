ea94968037ff81c42a8902374937ae50a75b4594
adnanrahin <adnan.rahin13@gmail.com>
2021-12-04 18:42:43 -0500

    update

--- src/LeetCode/Array/MaximumSubArray/Main.java
+++ src/LeetCode/Array/MaximumSubArray/Main.java
@@ -8,6 +8,19 @@ public class Main {
 
     public int maxSubArray(int[] nums) {
 
+        if (nums.length == 0) return 0;
+
+        int globalMaxSum = nums[0], localMaxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (localMaxSum + nums[i] < nums[i]) {
+                localMaxSum = nums[i];
+            }
+            localMaxSum += nums[i];
+            globalMaxSum = Math.max(globalMaxSum, localMaxSum);
+        }
+
+        return globalMaxSum;
     }
 
 }
