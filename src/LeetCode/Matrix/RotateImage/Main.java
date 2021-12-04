6c39c5a1472c6542cc54221b0c250b666dac674e
adnanrahin <adnan.rahin13@gmail.com>
2021-12-04 18:15:28 -0500

    Rotate Image

--- src/LeetCode/Matrix/RotateImage/Main.java
+++ src/LeetCode/Matrix/RotateImage/Main.java
@@ -4,11 +4,35 @@ public class Main {
 
     public static void main(String[] args) {
 
+        rotate(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+
     }
 
-    public void rotate(int[][] matrix) {
+    public static void rotate(int[][] matrix) {
 
-        
+        int row = matrix.length, col = matrix[0].length;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = i; j < col; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        int left = 0, right = matrix.length -1;
+
+        while (left < right) {
+
+            for (int i = 0; i < row; i++) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+            }
+
+            left++;
+            right--;
+        }
 
     }
 
