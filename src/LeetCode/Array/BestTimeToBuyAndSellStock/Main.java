03548bbc8b5e4c1362927f992eb31b8a906dd857
adnanrahin <adnan.rahin13@gmail.com>
2021-12-05 03:58:53 -0500

    Best time to buy stock

--- src/LeetCode/Array/BestTimeToBuyAndSellStock/Main.java
+++ src/LeetCode/Array/BestTimeToBuyAndSellStock/Main.java
@@ -8,8 +8,12 @@ public class Main {
 
     public static int maxProfit(int[] prices) {
 
-        int maxProfit = 0;
+        int maxProfit = 0, buy = prices[0];
 
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) buy = prices[i];
+            maxProfit = Math.max(maxProfit, prices[i] - buy);
+        }
 
         return maxProfit;
     }
