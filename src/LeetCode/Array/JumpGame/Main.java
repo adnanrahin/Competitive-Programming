9bf6b1bdc4bb83a54dddfad2819d0dcf160f5249
adnanrahin <adnan.rahin13@gmail.com>
2021-12-05 02:54:31 -0500

    JumpGame

---
+++ src/LeetCode/Array/JumpGame/Main.java
@@ -0,0 +1,22 @@
+package LeetCode.Array.JumpGame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean canJump(int[] nums) {
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            
+
+        }
+
+        return false;
+    }
+
+}
