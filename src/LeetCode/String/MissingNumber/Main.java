200cd767043a9ea76b8fc9b82d2291d9ad87d6b1
adnanrahin <adnan.rahin13@gmail.com>
2021-12-14 20:22:00 -0500

    Missing Number

---
+++ src/LeetCode/String/MissingNumber/Main.java
@@ -0,0 +1,21 @@
+package LeetCode.String.MissingNumber;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int missingNumber(int[] nums) {
+
+        int number = 0, sum = 0, arrSum = 0;
+
+        for (int i = 1; i <= nums.length; i++) {
+            sum += i;
+            arrSum += nums[i - 1];
+        }
+
+        return sum - arrSum;
+    }
+
+}
