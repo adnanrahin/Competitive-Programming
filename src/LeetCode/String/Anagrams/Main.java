d7d55357b6244bb912ae40d75cf0a18db9ea39db
adnanrahin <adnan.rahin13@gmail.com>
2021-12-14 19:46:08 -0500

    Update

---
+++ src/LeetCode/String/Anagrams/Main.java
@@ -0,0 +1,22 @@
+package LeetCode.String.Anagrams;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isAnagram(String s, String t) {
+        int[] arr = new int[26];
+        if(s.length() != t.length()) return false;
+        for(int i = 0; i < s.length(); i++){
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(arr[i] != 0) return false;
+        }
+        return true;
+    }
+
+}
